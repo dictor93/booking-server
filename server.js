39654ddb207fe3cfc3df9fab8cc9e87275059d03
zetov.ruslan <zetov.ruslan@gmail.com>
2019-03-10 01:08:46 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const dbClient = require("mongodb").MongoClient;
+const app = express();
+const port = 3000;
+app.get(/hotels(\/d)?/, (request, response) => {
+    console.log(request.url.split('/'));
+    const { url } = request;
+    const hotelId = url.split('/')[2];
+    console.log(hotelId);
+    dbClient.connect("mongodb://localhost:27017/", function(err, client){
+	    if(err){
+            response.statusCode = 500;
+            response.send()
+        }else{
+            const db = client.db('Booking');
+            const collection = db.collection('Hotels'); 
+            collection.find(hotelId?{id: parseInt(hotelId)}: {})
+            .toArray(function(err,docs){
+                if(err){
+                    console.log('err',err);
+                }else{
+                    response.json(docs);
+                    console.log(docs);
+                }
+            });
+        }
+    });
+});
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+    console.log(`server is listening on ${port}`)
+})
