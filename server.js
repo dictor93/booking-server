b24d5821f9b4e11ccce4268c110c92250808b337
zetov.ruslan <zetov.ruslan@gmail.com>
2019-03-10 02:11:37 +0200

    port change

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dbClient = require("mongodb").MongoClient;
 const app = express();
-const port = 80;
+const port = 8080;
 app.get('/',(req, res) => {
     res.send('Server is work');
 });
