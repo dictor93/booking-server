f55349d0b38d04e6ab342ff1141761bd796127bd
Viktor Limishenko <dictor93@gmail.com>
2019-03-26 04:01:06 +0200

    Set up server

---
+++ old_server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const dbClient = require("mongodb").MongoClient;
+const cors = require('cors');
+const app = express();
+const port = 8080;
+app.use(cors());
+app.get('/',(req, res) => {
+    res.send('Server is work');
+});
+app.get(/hotels(\/d)?/, (request, response) => {
+    console.log(request.url.split('/'));
+    const { url } = request;
+    const hotelId = url.split('/')[2];
+    console.log(hotelId);
+    dbClient.connect("mongodb://localhost:27017/", function(err, client){
+	    if(err){
+            response.statusCode = 500;
+            response.send()
+        }else{
+            const db = client.db('Booking');
+            const collection = db.collection('Hotels'); 
+            collection.find(hotelId?{id: parseInt(hotelId)}: {})
+            .toArray(function(err,docs){
+                if(err){
+                    console.log('err',err);
+                }else{
+                    response.json(docs);
+                    console.log(docs);
+                }
+            });
+        }
+    });
+});
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+    console.log(`server is listening on ${port}`)
+})
